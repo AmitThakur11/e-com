a483a47b3fea295b7901081e2cd6871b757e8e6e
amit thakur <amithakur081@gmail.com>
2022-03-04 19:53:03 +0530

    Add api

--- src/utils/axiosInitializer.js
+++ src/utils/axiosInitializer.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 export const axiosInitializer = ()=>{
-    axios.defaults.baseURL = "http://localhost:3002/"
+    axios.defaults.baseURL = "https://soulmadeapi.herokuapp.com/"
     // axios.defaults.headers.common["Authorization"] = localStorage.getItem("token");
 }
 
